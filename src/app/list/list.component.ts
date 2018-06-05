0a0e67750baca88e33068b7f28e24f094f777a7b
FranckAchard <36164145+FranckAchard@users.noreply.github.com>
2018-06-05 17:46:08 +0200

    sav

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../user.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  public users;
 
-  constructor() { }
+  constructor(public userService: UserService) { }
 
   ngOnInit() {
     // le composant est prêt (à être utilisé)
+    this.users = this.userService.users;
   }
+selectUser() {
+
+}
 
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -5,5 +5,26 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
+  users = [
+    {
+      'id': 0,
+      'name': 'Aisha Eichmann',
+      'image': 'https://s3.amazonaws.com/uifaces/faces/twitter/malykhinv/128.jpg',
+      'jobTitle': 'Customer Division Assistant'
+    },
+    {
+      'id': 1,
+      'name': 'Calista Skiles',
+      'image': 'https://s3.amazonaws.com/uifaces/faces/twitter/nessoila/128.jpg',
+      'jobTitle': 'Dynamic Solutions Orchestrator'
+    },
+    {
+      'id': 2,
+      'name': 'Kristofer Hermiston',
+      'image': 'https://s3.amazonaws.com/uifaces/faces/twitter/ernestsemerda/128.jpg',
+      'jobTitle': 'Dynamic Markets Planner'
+    }
+  ];
+
   constructor() { }
 }
